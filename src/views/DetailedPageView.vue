4e942206dfafa4f6ed29507efde4494734f0ca4d
JoMarianek <136638758+JoMarianek@users.noreply.github.com>
2024-06-04 23:48:52 +0200

    Rss ecomm 3 12 && rss ecomm 3 13 (#78)
    * init: detailed page
    
    * feat: set up routing and implement breadcrumbs
    
    * feat: implement carousel
    
    * feat: create product endpoint and interface
    
    * feat: call api for detailed product page
    
    * refactor: price into method
    
    * feat: remove mock pictures, fix error in types
    
    * fix: import statement in types file
    
    * refactor: switch curreny from pound to eur
    
    * feat: set up proper routing, fix commas and indentation
    
    * fix: format
    
    * feat: implement spinner for loading and redirect to 404
    
    * refactor: carousel for detailed product
    
    * feat: improve slider layout
    
    * feat: add tests for detailed product view and remove slider arrows for single image products
    
    * refactor: formatting
    
    * refactor: layout
    
    * init: modal slider
    
    * feat: work on implementing overlay carousel
    
    * feat: adjust opacity and background color of overlay
    
    * feat: fix overlay background color
    
    * feat: trying to make carousel work
    
    * feat: adjust overlay carousel sizing
    
    * refactor: change modal carousel layout
    
    * feat: implement modal close button
    
    * refactor: closing button for modal
    
    * refactor: indentation and quotes
    
    * refactor: indentation
    
    * refactor: indentation
    
    * refactor: adjust wrapper width

--- src/views/DetailedPageView.vue
+++ src/views/DetailedPageView.vue
@@ -5,7 +5,9 @@
         export default {
             data: () => ({
                 product: {} as ProductSingle,
-                loading: true
+                loading: true,
+                overlayVisible: false,
+                currentImageIndex: 0
             }),
             async mounted() {
                     const productID = this.$route.params.id as string;
@@ -26,7 +28,12 @@
                     const priceObj = this.product?.masterData?.current?.masterVariant?.prices?.find(
                         (elem) => elem.value.currencyCode === currencyCode
                     );
-                    return priceObj ? (priceObj.value.centAmount / 100).toFixed(2) : 'N/A';
+
+                    return priceObj ? (priceObj.value.centAmount / 100).toFixed(2) : "N/A";
+                },
+                showOverlay(index: number) {
+                    this.currentImageIndex = index;
+                    this.overlayVisible = true;
                 }
             },
             computed: {
@@ -41,7 +48,15 @@
                     return this.product.masterData?.current?.masterVariant?.images.map(image => {
                         return {
                             ...image,
-                            url: image.url.replace(/(\.[\w\d_-]+)$/i, '-large$1')
+                            url: image.url.replace(/(\.[\w\d_-]+)$/i, "-large$1")
+                        };
+                    }) || [];
+                },
+                extraLargeImages() {
+                    return this.product.masterData?.current?.masterVariant?.images.map(image => {
+                        return {
+                            ...image,
+                            url: image.url.replace(/(\.[\w\d_-]+)$/i, "-zoom$1")
                         };
                     }) || [];
                 },
@@ -68,9 +83,10 @@
                         :key="index"
                         class="carousel-item"
                         :src="image.url"
+                        @click="showOverlay(index)"
                     >
                         <v-img :src="image.url" class="carousel-image" alt="Product Image"></v-img>
-                </v-carousel-item>
+                    </v-carousel-item>
                 </v-carousel>
             </v-col>
             <v-col>
@@ -85,12 +101,31 @@
                 </v-row>
             </v-col>
         </v-row>
+        
+        <v-overlay class="overlay-content align-center justify-center" v-model="overlayVisible" opacity="1" scrim="rgba(0, 0, 0, 1)" contained persistent>
+            <v-row class="overlay-wrapper">
+                <v-btn @click="overlayVisible = false">
+                    x
+                </v-btn>
+                <v-carousel :show-arrows="showArrows">
+                    <v-carousel-item 
+                        v-for="(image, index) in largeImages"
+                        :key="index"
+                        class="modal-carousel-item"
+                        :src="image.url"
+                    >
+                        <v-img :src="image.url" class="overlay-image" alt="Enlarged Product Image" aspect-ratio="1"></v-img>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-row>
+        </v-overlay>
     </v-container>
 </template>
 
 <style scoped>
 .wrapper-container {
     overflow-x: hidden;
+    max-width: 1600px;
 }
 
 .carousel-item {
@@ -105,5 +140,31 @@
     max-width: 100%;
     max-height: 100%;
     object-fit: contain;
+    cursor: pointer;
+}
+
+.overlay-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+
+.modal-carousel-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    width: auto;
+}
+
+.overlay-image {
+    width: 50vw;
+    height: 80vh;
+    max-width: 80vw;
+    max-height: 80vh;
+    object-fit: contain;
+    margin: auto;
 }
 </style>
